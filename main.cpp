b7abf8727936271ddef99d21ffc36db83ad61b9a
sergeyiandronov <sergeyiandronov@gmail.com>
2017-10-30 15:05:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,38 +3,6 @@
 using namespace std;
 
 int main() {
-	unsigned int n;
-	
-	string line;
-	getline(cin,line);
-	istringstream numstream(line);
-	if(!(numstream>>n)){
-		cout<<"An error has occuried while reading input data.";
-		exit(0);
-	}
-	
-	getline(cin,line);
-	istringstream arrstream(line);
-	int *arr=new int[n];
-	for(int i=0;i<n;i++){
-		if(!(arrstream>>arr[i])){
-		        cout<<"An error has occuried while reading input data.";
-		        exit(0);
-		}
-		
-	}
-	
-	int lastindex=0;
-	while(lastindex!=n-1){
-		for(int i=lastindex;i<n;i++){
-			if(arr[lastindex]>arr[i]){
-				swap(arr[lastindex],arr[i]);
-				break;
-			}#include <iostream>
-#include <sstream>
-using namespace std;
-
-int main() {
     unsigned int n;
 
     string line;
